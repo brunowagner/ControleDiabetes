3696a6780adffa81354a315dc9b755fb7cf8104c
Bruno Wagner <brunowvmoraes@gmail.com>
2019-04-11 20:56:00 -0300

    Set RecyclerView of Bolus Calculate Configuration.

--- app/src/main/java/br/com/bwsystemssolutions/controlediabetes/ConfigCalcularBolus.java
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/ConfigCalcularBolus.java
@@ -4,9 +4,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusContract;
+import br.com.bwsystemssolutions.controlediabetes.adapter.BolusTimeBlockAdapter;
 import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusDBHelper;
 
 import static br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusContract.*;
@@ -14,7 +15,7 @@ import static br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusCont
 public class ConfigCalcularBolus extends AppCompatActivity {
 
     SQLiteDatabase mDb;
-    RecyclerView mDadosParaCalcuroReyclerView;
+    RecyclerView mRecyclerView;
     Cursor mCursor;
 
     @Override
@@ -22,7 +23,11 @@ public class ConfigCalcularBolus extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_calcular_bolus);
 
-        mDadosParaCalcuroReyclerView = (RecyclerView) findViewById(R.id.rv_dados_para_calculo);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_dados_para_calculo);
+        configureRecyclerView();
+
+
+
 
         // Configurando o banco de dados
         CalculoDeBolusDBHelper dbHelper = new CalculoDeBolusDBHelper(this);
@@ -34,6 +39,20 @@ public class ConfigCalcularBolus extends AppCompatActivity {
         return mDb.query(TimeBlockEntry.TABLE_NAME,
                 null, null, null, null,null,
                 TimeBlockEntry.COLUMN_INITIAL_TIME_NAME);
+    }
+
+    private void configureRecyclerView(){
+        BolusTimeBlockAdapter bolusTimeBlockAdapter = new BolusTimeBlockAdapter();
+        mRecyclerView.setAdapter(bolusTimeBlockAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        /*
+         * Use this setting to improve performance if you know that changes in content do not
+         * change the child layout size in the RecyclerView
+         */
+        mRecyclerView.setHasFixedSize(true);
     }
     
 }
