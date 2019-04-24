448eea7532185ccd5736631a11038f90f95bc600
Bruno Wagner <39104163+brunowagner@users.noreply.github.com>
2019-04-24 08:32:30 -0300

    Create RegistrosAdapter.java

---
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/adapter/RegistrosAdapter.java
@@ -0,0 +1,23 @@
+
+package br.com.bwsystemssolutions.controlediabetes.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import br.com.bwsystemssolutions.controlediabetes.R;
+import br.com.bwsystemssolutions.controlediabetes.classe.BolusTimeBlockData;
+import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusContract;
+import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusDBHelper;
+
+public class RegistrosAdapter extends RecyclerView.Adapter<RegistrosAdapter.RegistrosAdapterViewHolder> {
+}
