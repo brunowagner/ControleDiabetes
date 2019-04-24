7b0c66770b5c0c84786f402afc99ac9da736cd07
Bruno Wagner <39104163+brunowagner@users.noreply.github.com>
2019-04-24 09:46:31 -0300

    Add records and events table and rename timeblock table.
    Update CalculoDeBolusContract.java whit new tables

--- app/src/main/java/br/com/bwsystemssolutions/controlediabetes/data/CalculoDeBolusContract.java
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/data/CalculoDeBolusContract.java
@@ -8,15 +8,33 @@ import android.provider.BaseColumns;
 public class CalculoDeBolusContract {
 
     /**
-     * Cada classe interna que define uma tabela (neste caso é a tabela de bloco de tempo)
+     * Cada classe interna que define uma tabela (neste caso é a tabela de blocos de tempo)
      */
-    // COMPLETED (1) Criar uma classe interna que implementa a interface 'BaseColumns'
+    // classe interna que implementa a interface 'BaseColumns'
     public static  final class TimeBlockEntry implements BaseColumns{
-        //COMPLETED (2) criar as strings finais que definem as colunas
-        public static final String TABLE_NAME = "timeBlock";
+        //strings finais que definem as colunas
+        public static final String TABLE_NAME = "timeBlocks";
         public static final String COLUMN_INITIAL_TIME_NAME = "initialTime";
         public static final String COLUMN_RELATION_NAME = "relation";
         public static final String COLUMN_SENSITIVITY_FACTOR_NAME = "sensitivityFactor";
         public static final String COLUMN_TARGET_NAME = "target";
+    }
+    
+    public static  final class RecordEntry implements BaseColumns{
+        public static final String TABLE_NAME = "records";
+        public static final String COLUMN_DATE_TIME_NAME = "date_time";
+        public static final String COLUMN_GLUCOSE_NAME = "glucose";
+        public static final String COLUMN_EVENTO_NAME = "event";
+        public static final String COLUMN_CARBOHYDRATE_NAME = "carbohydrate";
+        public static final String COLUMN_FAST_INSULIN_NAME = "fast_insulin";
+        public static final String COLUMN_BASAL_INSULIN_NAME = "basal_insulin";
+        public static final String COLUMN_SICK_NAME = "sick";
+        public static final String COLUMN_MEDICAMENT_NAME = "medicament";
+        public static final String COLUMN_NOTE_NAME = "note";
+    }
+    
+    public static  final class EventEntry implements BaseColumns{
+        public static final String TABLE_NAME = "events";
+        public static final String COLUMN_EVENT_NAME = "event";
     }
 }
