b9fa30881653606578019c29a0c03e943d99b94e
Bruno Wagner <39104163+brunowagner@users.noreply.github.com>
2019-04-26 11:04:48 -0300

    Implemented clickHandler on recyclerView.

--- app/src/main/java/br/com/bwsystemssolutions/controlediabetes/RegistrosActivity.java
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/RegistrosActivity.java
@@ -9,7 +9,7 @@ import br.com.bwsystemssolutions.controlediabetes.adapter.BolusTimeBlockAdapter;
 import br.com.bwsystemssolutions.controlediabetes.adapter.RegistrosAdapter;
 import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusDBHelper;
 
-public class RegistrosActivity extends AppCompatActivity {
+public class RegistrosActivity extends AppCompatActivity implements RecordAdapterOnClickHandler{
     RecyclerView mRegistrosRecyclerView;
     RegistrosAdapter mRegistrosAdapter;
 
@@ -37,6 +37,49 @@ public class RegistrosActivity extends AppCompatActivity {
     }
 
     private void refreshRecyclerView(){
-        mRegistrosAdapter.refreshData();                  //migracao do mDB
+        mRegistrosAdapter.refreshData();
+    }
+	
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu_records_operation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_record_add:
+				Context context = this;
+                Intent intent = new Intent(context, RecordDetailActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                super.onOptionsItemSelected(item);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+	
+	//implementação do RecordAdapterOnClickHandler
+    @Override
+    public void onClick(Record record) {
+        Context context = this;
+        Intent intent = new Intent(context, RecordDetailActivity.class);
+
+        //Empacotando o objeto
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(Record.BUNDLE_STRING_KEY, record);
+
+        //passando o objeto na Intent
+        intent.putExtras(bundle);
+
+        startActivity(intent);
     }
 }
