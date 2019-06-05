496e582d7d865e3be917639266f39744de4829e5
Bruno Wagner <brunowvmoraes@gmail.com>
2019-06-04 23:58:07 -0300

    Implements fixed preference by external api.

--- app/src/main/java/br/com/bwsystemssolutions/controlediabetes/SettingsFragment.java
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/SettingsFragment.java
@@ -1,64 +1,37 @@
 package br.com.bwsystemssolutions.controlediabetes;
 
 
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.preference.EditTextPreference;
-import android.support.v7.preference.EditTextPreferenceDialogFragmentCompat;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceFragmentCompat;
-import android.support.v7.preference.PreferenceManager;
-import android.text.InputType;
+import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toast;
 
-import br.com.bwsystemssolutions.controlediabetes.preferenceV7Fix.EditTextPreferenceDialogFragmentCompatFixed;
 
-public class SettingsFragment extends PreferenceFragmentCompat implements PreferenceFragmentCompat.OnPreferenceDisplayDialogCallback  {
+import com.takisoft.fix.support.v7.preference.EditTextPreference;
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat   {
 
     private EditTextPreference mglicemiaBaixaEditText;
 
     @Override
-    public void onCreatePreferences(Bundle bundle, String s) {
-        addPreferencesFromResource(R.xml.pref_main);
+    public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.pref_main, rootKey);
 
         mglicemiaBaixaEditText = (EditTextPreference) findPreference("glicemia_Baixa");
+        int i = mglicemiaBaixaEditText.getEditText().getInputType();
+        Log.d("bwvm", "onCreatePreferences: "+ i);
     }
 
-    @Override
-    public void onDisplayPreferenceDialog(Preference preference) {
-
-        String key = preference.getKey();
-        Log.d("bwvm", "onDisplayPreferenceDialog: key" + key);
-        if (preference instanceof EditTextPreference){
-            EditTextPreferenceDialogFragmentCompatFixed f;
-            f = EditTextPreferenceDialogFragmentCompatFixed.newInstance(key,InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
-
-            f.setTargetFragment(this, 0);
-            f.show(getFragmentManager(),"android.support.v7.preference.PreferenceFragment.DIALOG");
-        } else {
-            super.onDisplayPreferenceDialog(preference);
-        }
-    }
-
-
-    @Override
-    public boolean onPreferenceDisplayDialog(@NonNull PreferenceFragmentCompat preferenceFragmentCompat, Preference preference) {
-        String key = preference.getKey();
-        Log.d("bwvm", "onPreferenceDisplayDialog: key" + key);
-        if (preference instanceof EditTextPreference){
-            EditTextPreferenceDialogFragmentCompatFixed f;
-            f = EditTextPreferenceDialogFragmentCompatFixed.newInstance(key,InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
-
-            f.setTargetFragment(this, 0);
-            f.show(getFragmentManager(),"android.support.v7.preference.PreferenceFragment.DIALOG");
-            return true;
-        }
-
-        return false;
-    }
-
-
+//    @Override
+//    public void onCreatePreferences(Bundle bundle, String s) {
+//        //addPreferencesFromResource(R.xml.pref_main);
+//        setPreferencesFromResource(R.xml.pref_main, );
+//
+//        mglicemiaBaixaEditText = (EditTextPreference) findPreference("glicemia_Baixa");
+//        int i = mglicemiaBaixaEditText.getEditText().getInputType();
+//        Log.d("bwvm", "onCreatePreferences: "+ i);
+//    }
 
 }
