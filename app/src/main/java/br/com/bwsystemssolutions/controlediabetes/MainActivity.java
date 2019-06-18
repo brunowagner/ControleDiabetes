2e44a145a6d95dca9680c7eed9a6bc2d2af00d8b
Bruno Wagner <brunowvmoraes@gmail.com>
2019-06-17 22:25:43 -0300

    Add code to test FileDialog.

--- app/src/main/java/br/com/bwsystemssolutions/controlediabetes/MainActivity.java
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/MainActivity.java
@@ -1,12 +1,16 @@
 package br.com.bwsystemssolutions.controlediabetes;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.DocumentsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,9 +21,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 
 import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusDBHelper;
+import br.com.bwsystemssolutions.controlediabetes.data.directoryOrFileChoose.FileDialog;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity{
     Button mRegistrosButton;
     Button mImportarDB;
     SQLiteDatabase mDb;
+    FileDialog mFileDialog;
 
     public final static int PICKFILE_REQUEST_CODE = 1111;
 
@@ -72,10 +79,52 @@ public class MainActivity extends AppCompatActivity{
         //TODO - https://stackoverflow.com/questions/6540906/simple-export-and-import-of-a-sqlite-database-on-android (usei este)
         //TODO - https://gist.github.com/granoeste/5574148 (explica as pastas)
 
+        testar_filedialog();
 
 
+    }
 
+    private void testar_filedialog(){
+        Log.d("bwvm", "onCreate: Main: Environment.getExternalStorageDirectory().getAbsolutePath() = " + Environment.getExternalStorageDirectory());
+        Log.d("bwvm", "onCreate: Main: Environment.getExternalStorageDirectory().getAbsolutePath() =  " + Environment.getExternalStorageDirectory().getAbsolutePath());
 
+        // Bloco colocado para poder verificar a permissáo de leitura e escrita na memoria externa.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            Log.d("bwvm", "onCreate: não tem permissao para ler no Storage");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        10001);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            Log.d("bwvm", "onCreate: não tem permissao para escrever no Storage");
+        }
+
+        //Codigo conforme exemplo da fonte
+        File mPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android");
+        //File mPath = new File(getExternalFilesDir(Environment.getDataDirectory().getAbsolutePath()).getAbsolutePath());
+        //File mPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "//BW");
+        mFileDialog = new FileDialog(this, mPath );
+        mFileDialog.addFileListener(new FileDialog.FileSelectedListener() {
+            @Override
+            public void fileSelected(File file) {
+                Log.d("bwvm", "selected file " + file.toString());
+            }
+        });
+
+        mFileDialog.addDirectoryListener(new FileDialog.DirectorySelectedListener() {
+            @Override
+            public void directorySelected(File directory) {
+                Log.d("bwvm", "selected dir " + directory.toString());
+            }
+        });
+
+        mFileDialog.setSelectDirectoryOption(true);
+
+        mFileDialog.showDialog();
     }
 
     // Menu code block -------------------------------------------
@@ -114,6 +163,7 @@ public class MainActivity extends AppCompatActivity{
 
         super.onActivityResult(requestCode, resultCode, data);
     }
+
 
 
 
@@ -156,14 +206,15 @@ public class MainActivity extends AppCompatActivity{
 //            intent.setType("file/*");
 //            startActivityForResult(intent, PICKFILE_REQUEST_CODE);
 
-            Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-            i.addCategory(Intent.CATEGORY_DEFAULT);
-            startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999);
+            //Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            //i.addCategory(Intent.CATEGORY_DEFAULT);
+            //startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999);
 
 //            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
 //            intent.addCategory(Intent.CATEGORY_OPENABLE);
 //            intent.setType("*/*");
 //            startActivityForResult(intent, 8888);
+            mFileDialog.showDialog();
         }
 
 
@@ -183,5 +234,7 @@ public class MainActivity extends AppCompatActivity{
 
             startActivity(intent);
         }
+
+
     }
 }
