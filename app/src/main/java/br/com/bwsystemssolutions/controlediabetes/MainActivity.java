76476021684d3e9c34b9de19615200d3f2646aed
Bruno Wagner <brunowvmoraes@gmail.com>
2019-07-03 20:31:50 -0300

    remove implement a folder selector from internet fragments.

--- app/src/main/java/br/com/bwsystemssolutions/controlediabetes/MainActivity.java
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/MainActivity.java
@@ -1,48 +1,28 @@
 package br.com.bwsystemssolutions.controlediabetes;
 
-import android.Manifest;
-import android.app.Dialog;
+
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusDBHelper;
-import br.com.bwsystemssolutions.controlediabetes.data.customFileExplorer.MyListAdapter;
-import br.com.bwsystemssolutions.controlediabetes.data.directoryOrFileChoose.DialogChooseDirectory;
-import br.com.bwsystemssolutions.controlediabetes.data.directoryOrFileChoose.FileDialog;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     Button mCalculoDeBolusButton;
     Button mRegistrosButton;
     Button mImportarDB;
     SQLiteDatabase mDb;
-    FileDialog mFileDialog;
-
-    public final static int PICKFILE_REQUEST_CODE = 1111;
 
 
     @Override
@@ -88,80 +68,8 @@ public class MainActivity extends AppCompatActivity{
         //TODO - https://gist.github.com/granoeste/5574148 (explica as pastas)
         //TODO - https://code.google.com/archive/p/android-file-dialog/source/default/source (repositorio do google) ()encontrado pelo link abaixo
         //TODO - https://stackoverflow.com/questions/8587325/how-to-select-folder-in-android/8883886 (me levou ao repositorio acima)
-
-        //testar_filedialog();
-        //testarDialogChooseDirectory();
-        testeCustomFileExplorer();
-
-
-
     }
 
-    public void testeCustomFileExplorer(){
-        Intent intent = new Intent(MainActivity.this, CustomFileExplorerActivity.class);
-        startActivity(intent);
-    }
-
-
-
-
-
-
-    class MyOnChooseDialog implements DialogChooseDirectory.Result{
-
-        @Override
-        public void onChooseDirectory(String dir) {
-            Log.d("bwvm", "onChooseDirectory: Diretório escolhido = " + dir);
-        }
-    }
-
-    private void testarDialogChooseDirectory(){
-        MyOnChooseDialog res = new MyOnChooseDialog();
-        DialogChooseDirectory dcd = new DialogChooseDirectory(this, res, getExternalFilesDir(Environment.getDataDirectory().getAbsolutePath()).getAbsolutePath());
-    }
-
-    private void testar_filedialog(){
-        Log.d("bwvm", "onCreate: Main: Environment.getExternalStorageDirectory().getAbsolutePath() = " + Environment.getExternalStorageDirectory());
-        Log.d("bwvm", "onCreate: Main: Environment.getExternalStorageDirectory().getAbsolutePath() =  " + Environment.getExternalStorageDirectory().getAbsolutePath());
-
-        // Bloco colocado para poder verificar a permissáo de leitura e escrita na memoria externa.
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            Log.d("bwvm", "onCreate: não tem permissao para ler no Storage");
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        10001);
-            }
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            Log.d("bwvm", "onCreate: não tem permissao para escrever no Storage");
-        }
-
-        //Codigo conforme exemplo da fonte
-        File mPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android");
-        //File mPath = new File(getExternalFilesDir(Environment.getDataDirectory().getAbsolutePath()).getAbsolutePath());
-        //File mPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "//BW");
-        mFileDialog = new FileDialog(this, mPath );
-        mFileDialog.addFileListener(new FileDialog.FileSelectedListener() {
-            @Override
-            public void fileSelected(File file) {
-                Log.d("bwvm", "selected file " + file.toString());
-            }
-        });
-
-        mFileDialog.addDirectoryListener(new FileDialog.DirectorySelectedListener() {
-            @Override
-            public void directorySelected(File directory) {
-                Log.d("bwvm", "selected dir " + directory.toString());
-            }
-        });
-
-        mFileDialog.setSelectDirectoryOption(true);
-
-        mFileDialog.showDialog();
-    }
 
     // Menu code block -------------------------------------------
     @Override
@@ -175,7 +83,7 @@ public class MainActivity extends AppCompatActivity{
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_config_item){
+        if (id == R.id.action_config_item) {
             //Intent intent = new Intent(MainActivity.this, BolusCalculateConfig.class);
             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(intent);
@@ -201,24 +109,22 @@ public class MainActivity extends AppCompatActivity{
     }
 
 
-
-
     // Internal class to click handler --------------------------
-    class ListenerEvents implements View.OnClickListener{
+    class ListenerEvents implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
             int id = v.getId();
 
-            if (id == R.id.btn_calculo_de_bolus){
+            if (id == R.id.btn_calculo_de_bolus) {
                 goToCalculoDeBolus();
             }
 
-            if (id == R.id.btn_registros){
+            if (id == R.id.btn_registros) {
                 goToRegistros();
             }
 
-            if (id == R.id.btn_import_db){
+            if (id == R.id.btn_import_db) {
                 performFileSearch();
 
 //                CalculoDeBolusDBHelper dbHelper = new CalculoDeBolusDBHelper(MainActivity.this);
@@ -235,27 +141,9 @@ public class MainActivity extends AppCompatActivity{
         }
 
         public void performFileSearch() {
-//            Intent intent = new Intent(Intent.ACTION_PICK);
-//            startActivityForResult(intent,PICKFILE_REQUEST_CODE);
-
-//            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//            intent.setType("file/*");
-//            startActivityForResult(intent, PICKFILE_REQUEST_CODE);
-
-            //Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-            //i.addCategory(Intent.CATEGORY_DEFAULT);
-            //startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999);
-
-//            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-//            intent.addCategory(Intent.CATEGORY_OPENABLE);
-//            intent.setType("*/*");
-//            startActivityForResult(intent, 8888);
-            mFileDialog.showDialog();
         }
 
-
-
-        void goToCalculoDeBolus(){
+        void goToCalculoDeBolus() {
             Context context = MainActivity.this;
             Class destinationClass = CalcularBolus.class;
             Intent intent = new Intent(context, destinationClass);
@@ -263,14 +151,12 @@ public class MainActivity extends AppCompatActivity{
             startActivity(intent);
         }
 
-        void goToRegistros(){
+        void goToRegistros() {
             Context context = MainActivity.this;
             Class destinationClass = RegistrosActivity.class;
             Intent intent = new Intent(context, destinationClass);
 
             startActivity(intent);
         }
-
-
     }
 }
