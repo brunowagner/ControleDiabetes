8b022240489c1d91c35c9d45d840657a902514c0
Bruno Wagner <brunowvmoraes@gmail.com>
2019-08-21 10:40:53 -0300

    Crated layout to bolus table.

---
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/BolusTableActivity.java
@@ -0,0 +1,13 @@
+package br.com.bwsystemssolutions.controlediabetes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BolusTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bolus_table);
+    }
+}
