66bfcf85ea2d4bca4ced0837e0425d1ef8a89b1e
Bruno Wagner <brunowvmoraes@gmail.com>
2019-12-13 15:26:36 -0300

    Create Bolus class.

---
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/classe/Bolus.java
@@ -0,0 +1,40 @@
+package br.com.bwsystemssolutions.controlediabetes.classe;
+
+public class Bolus {
+    private int id;
+    private int glucose;
+    private int meal_id;
+    private double bolus;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGlucose() {
+        return glucose;
+    }
+
+    public void setGlucose(int glucose) {
+        this.glucose = glucose;
+    }
+
+    public int getMeal_id() {
+        return meal_id;
+    }
+
+    public void setMeal_id(int meal_id) {
+        this.meal_id = meal_id;
+    }
+
+    public double getBolus() {
+        return bolus;
+    }
+
+    public void setBolus(double bolus) {
+        this.bolus = bolus;
+    }
+}
