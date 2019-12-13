12a7b7bfc2527e92504143d54d04d711fc707fd0
Bruno Wagner <brunowvmoraes@gmail.com>
2019-12-13 16:01:40 -0300

    Codding BolusDAO.

---
+++ app/src/main/java/br/com/bwsystemssolutions/controlediabetes/data/dao/BolusDAO.java
@@ -0,0 +1,86 @@
+package br.com.bwsystemssolutions.controlediabetes.data.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+
+import br.com.bwsystemssolutions.controlediabetes.classe.Bolus;
+import br.com.bwsystemssolutions.controlediabetes.classe.Meal;
+import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusContract;
+import br.com.bwsystemssolutions.controlediabetes.data.CalculoDeBolusDBHelper;
+
+public class BolusDAO {
+    private CalculoDeBolusDBHelper dbHelper;
+    private String TABLE_NAME = CalculoDeBolusContract.BolusEntry.TABLE_NAME;
+    private String COLUMN_ID_NAME = CalculoDeBolusContract.BolusEntry._ID;
+
+    public BolusDAO(Context context){
+        dbHelper = new CalculoDeBolusDBHelper(context);
+    }
+
+    public ArrayList<Bolus> fetchAll(){
+        final SQLiteDatabase db = dbHelper.getReadableDatabase();
+        final Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " ORDER BY " + CalculoDeBolusContract.BolusEntry.COLUMN_GLUCOSE_NAME,null);
+        return parseToBolus(cursor);
+    }
+
+    public boolean add (Bolus bolus){
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues cv = parseToContentValues(bolus);
+        boolean inserted = db.insert(TABLE_NAME, null, cv) > 0;
+        db.close();
+        return inserted;
+    }
+
+    public boolean delete (int id){
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String whereClause = COLUMN_ID_NAME + " = ?";
+        String[] whereArgs = new String[] {String.valueOf(id)};
+        boolean deleted = db.delete(TABLE_NAME, whereClause, whereArgs ) > 0;
+        db.close();
+        return deleted;
+    }
+
+    public boolean deleteByGlucose (int glucose){
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String whereClause = CalculoDeBolusContract.BolusEntry.COLUMN_GLUCOSE_NAME + " = ?";
+        String[] whereArgs = new String[] {String.valueOf(glucose)};
+        boolean deleted = db.delete(TABLE_NAME, whereClause, whereArgs ) > 0;
+        db.close();
+        return deleted;
+    }
+
+    public boolean update (Bolus bolus){
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues cv = parseToContentValues(bolus);
+        return db.update (TABLE_NAME, cv, COLUMN_ID_NAME + " = ?", new String[] {bolus.getId() + ""}) > 0;
+    }
+
+    private ArrayList<Bolus> parseToBolus(Cursor cursor){
+        ArrayList<Bolus> boluss = new ArrayList<>();
+
+        while(cursor.moveToNext()){
+            Bolus bolus = new Bolus();
+            bolus.setId(cursor.getInt(cursor.getColumnIndex(CalculoDeBolusContract.BolusEntry._ID)));
+            bolus.setGlucose(cursor.getInt(cursor.getColumnIndex(CalculoDeBolusContract.BolusEntry.COLUMN_GLUCOSE_NAME)));
+            bolus.setMeal_id(cursor.getInt(cursor.getColumnIndex(CalculoDeBolusContract.BolusEntry.COLUMN_MEAL_ID_NAME)));
+            bolus.setBolus(cursor.getDouble(cursor.getColumnIndex(CalculoDeBolusContract.BolusEntry.COLUMN_BOLUS_NAME)));
+            boluss.add(bolus);
+        }
+        cursor.close();
+        return boluss;
+    }
+
+    @NonNull
+    private ContentValues parseToContentValues(Bolus bolus) {
+        ContentValues cv = new ContentValues();
+        cv.put(CalculoDeBolusContract.BolusEntry.COLUMN_GLUCOSE_NAME, bolus.getGlucose());
+        cv.put(CalculoDeBolusContract.BolusEntry.COLUMN_MEAL_ID_NAME, bolus.getMeal_id());
+        cv.put(CalculoDeBolusContract.BolusEntry.COLUMN_BOLUS_NAME, bolus.getBolus());
+        return cv;
+    }
+}
